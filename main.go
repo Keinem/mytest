909841bc5bc8b19078cb8e4cae217bf4e24c33c1
Keinem <seventh_arrow@hotmail.com>
2017-03-15 23:02:59 +0900

    commit 2017/3/15

--- main.go
+++ main.go
@@ -40,32 +40,20 @@ func pinv(base []float64, i, j int) mat64.Dense {
 }
 
 func main() {
-	// elem := []float64{2, 5, 1, 3}
-	// x := mat64.NewDense(2, 2, elem)
 
 	elem2 := []float64{2, 4, 1, 2}
-	x2 := mat64.NewDense(2, 2, elem2)
+	x := mat64.NewDense(2, 2, elem2)
 
 	rets := pinv(elem2, 2, 2)
 	spew.Println(rets)
 
 	var invck mat64.Dense
-	invx1 := invck.Inverse(x2)
+	invx1 := invck.Inverse(x)
 	if invx1 != nil {
 		spew.Println(invx1)
 	}
 	fa := mat64.Formatted(&invck, mat64.Prefix("    "), mat64.Squeeze())
 	fmt.Printf("INV TEST:\na = %v\n\n", fa)
-
-	var m3 mat64.SVD
-	m3.Factorize(x2, matrix.SVDFull)
-	var um2, vm2, s2m, s3m, inv, inv2 mat64.Dense
-	um2.UFromSVD(&m3)
-	vm2.VFromSVD(&m3)
-	inv.Inverse(&um2)
-	s2m.Mul(&inv, x2)
-	s3m.Mul(&s2m, &vm2)
-	inv2.Inverse(&s3m)
 
 	data := make([]float64, 36)
 	for i := range data {
